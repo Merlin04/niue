e0a6622a92076def7bfa68377aaa671bbf13203b
Benjamin Smith <Merlin04@users.noreply.github.com>
2021-09-29 17:29:24 +0000

    Export useRerender

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import s from "./state";
 import c from "./events";
+import { useRerender as u } from "./utils";
 
 export const createState = s;
 export const createEvent = c;
+export const useRerender = u;
